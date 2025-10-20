a90ca3909c91c9a312b78a6a14ba6572c2de8f3d
Javier Celada <celada.jcm.javier@gmail.com>
2025-10-20 10:55:58 +0200

    Loop hours

--- main.py
+++ main.py
@@ -22,73 +22,18 @@ if wlan is None:
         pass  # you shall not pass :D
 
 # Main Code goes here, wlan is a working network.WLAN(STA_IF) instance.
-print("ESP OK")
+print("Internet Connection OK")
 
-localtime = time.localtime()
-today = "{}-{}-{}".format(localtime[0], localtime[1], localtime[2])
-print("Today's date:", today)
-
-print("Fetching ESIOS data: ", end="")
-url = "https://api.esios.ree.es/archives/70/download_json?locale=es&date=" + today
-resp = urequests.get(url)
-data = resp.json()
-print("Data fetched!")
-
-current_hour = localtime[3] + 2  # Adjust for timezone if necessary
-print(f"Current hour: {current_hour}h")
-current_price = None
-next_price = None
-prices = []
-
-for item in data["PVPC"]:
-  hour = int(item["Hora"].split("-")[0])
-  price = float(item["PCB"].replace(",", "."))
-  prices.append(price)
-  if hour == current_hour:
-    current_price = price
-    print(f"Current hour price: {price} €/MWh")
-  if hour == current_hour + 1:
-    next_price = price
-    print(f"Next hour price: {price} €/MWh")
-
-sorted_prices = sorted(prices)
-blue = sorted_prices[0]
-print(f"Today's minimum {blue} €/MWh ------ BLUE")
-green = sorted_prices[5]
-print(f"Today's next minimum {sorted_prices[5]} €/MWh ------ GREEN")
-yellow = sorted_prices[11]
-print(f"Today's mid {sorted_prices[11]} €/MWh ------ YELLOW")
-orange = sorted_prices[17]
-print(f"Today's previous maximum {sorted_prices[17]} €/MWh ------ ORANGE")
-red = sorted_prices[23]
-print(f"Today's maximum {sorted_prices[23]} €/MWh ------ RED")
-
-def price_color(price):
-  if price == red:
-    return gc9a01.RED
-  elif red > price >= orange:
-    return gc9a01.ORANGE
-  elif orange > price >= yellow:
-    return gc9a01.YELLOW
-  elif yellow > price >= green:
-    return gc9a01.GREEN
-  else:
-    return gc9a01.BLUE
-
-current_color = price_color(current_price)
-print(f"Current price color: {current_color}")
-next_color = price_color(next_price)
-print(f"Next price color: {next_color}")
-
+previous_hour = -1
 def center(font, s, row, color=gc9a01.WHITE):
-    screen = tft.width()                     # get screen width
-    width = tft.write_len(font, s)           # get the width of the string
-    if width and width < screen:             # if the string < display
-        col = tft.width() // 2 - width // 2  # find the column to center
-    else:                                    # otherwise
-        col = 0                              # left justify
+        screen = tft.width()                     # get screen width
+        width = tft.write_len(font, s)           # get the width of the string
+        if width and width < screen:             # if the string < display
+            col = tft.width() // 2 - width // 2  # find the column to center
+        else:                                    # otherwise
+            col = 0                              # left justify
 
-    tft.write(font, s, col, row, color)      # and write the string
+        tft.write(font, s, col, row, color)      # and write the string
 
 tft = gc9a01.GC9A01(
     SPI(2, baudrate=80000000, polarity=0, sck=Pin(10), mosi=Pin(11)),
@@ -104,18 +49,80 @@ tft = gc9a01.GC9A01(
 tft.init()
 tft.fill(gc9a01.BLACK)
 
-# center the name of the first font, using the font
-row = 60
-center(noto_sans, "CURRENT", row, current_color)
-row += noto_sans.HEIGHT
-center(noto_sans, f"{current_price} €/MWh", row, current_color)
-row += noto_sans.HEIGHT
+def get_data():
+    print("Fetching ESIOS data: ", end="")
+    url = "https://api.esios.ree.es/archives/70/download_json?locale=es&date=" + today
+    resp = urequests.get(url)
+    data = resp.json()
+    print("Data fetched!")
 
-# center the name of the second font, using the font
-center(noto_sans, "NEXT", row, next_color)
-row += noto_sans.HEIGHT
-center(noto_sans, f"{next_price} €/MWh", row, next_color)
-row += noto_sans.HEIGHT
+    current_price = None
+    next_price = None
+    prices = []
+
+    for item in data["PVPC"]:
+      hour = int(item["Hora"].split("-")[0])
+      price = float(item["PCB"].replace(",", "."))
+      prices.append(price)
+      if hour == current_hour:
+        current_price = price
+        print(f"Current hour price: {price} €/MWh")
+      if hour == current_hour + 1:
+        next_price = price
+        print(f"Next hour price: {price} €/MWh")
+
+    sorted_prices = sorted(prices)
+    blue = sorted_prices[0]
+    print(f"Today's minimum {blue} €/MWh ------ BLUE")
+    green = sorted_prices[5]
+    print(f"Today's next minimum {green} €/MWh ------ GREEN")
+    yellow = sorted_prices[11]
+    print(f"Today's mid {yellow} €/MWh ------ YELLOW")
+    orange = sorted_prices[17]
+    print(f"Today's previous maximum {orange} €/MWh ------ ORANGE")
+    red = sorted_prices[23]
+    print(f"Today's maximum {red} €/MWh ------ RED")
+    
+    def price_color(price):
+      if price == red:
+        return gc9a01.RED
+      elif red > price >= orange:
+        return gc9a01.color565(255,165,0)
+      elif orange > price >= yellow:
+        return gc9a01.YELLOW
+      elif yellow > price >= green:
+        return gc9a01.GREEN
+      else:
+        return gc9a01.BLUE
+
+    current_color = price_color(current_price)
+    print(f"Current price color: {current_color}")
+    next_color = price_color(next_price)
+    print(f"Next price color: {next_color}")
+
+    # center the name of the first font, using the font
+    row = 60
+    center(noto_sans, "CURRENT", row, current_color)
+    row += noto_sans.HEIGHT
+    center(noto_sans, f"{current_price} EUR / MWh", row, current_color)
+    row += noto_sans.HEIGHT
+
+    # center the name of the second font, using the font
+    center(noto_sans, "NEXT", row, next_color)
+    row += noto_sans.HEIGHT
+    center(noto_sans, f"{next_price}", row, next_color)
+    row += noto_sans.HEIGHT
+
+# Main
+while True:
+    localtime = time.localtime()
+    today = "{}-{}-{}".format(localtime[0], localtime[1], localtime[2])
+    current_hour = localtime[3] # Adjust for timezone if necessary
+    if (current_hour != previous_hour):
+        print("Today's date:", today)
+        previous_hour = current_hour
+        print(f"Current hour: {current_hour}h")
+        get_data()
 
 def web_page():
   if led.value() == 1:
